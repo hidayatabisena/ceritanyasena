41aae4bf71745934808404e9555d978c4cbcefb1
Han Sena <hansena@moyahexagon.com>
2024-11-05 08:56:31 +0700

    Use Astro Image component for better image caching

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,12 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+	site: 'https://hidayatabisena.com',
+	integrations: [mdx({
+		components: {
+			'Image': 'astro:assets'
+		}
+	}), sitemap()],
 	scripts: [
 		{
 			type: 'module',
